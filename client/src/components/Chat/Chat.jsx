02e5be7b0d2484547a12870724267abec95e4c35
Sourabh Singh <sourabh.singh@zs.com>
2025-04-26 12:51:34 +0530

    added DB changes

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Chat.css";
-
+import ChatResponseTable from "../Table/Table.jsx";
 function Chat() {
   const [input, setInput] = useState("");
   const [chat, setChat] = useState([]);
@@ -34,27 +34,31 @@ function Chat() {
   };
 
   return (
-    <div className="Chatbot">
-      <div className="chat-box">
-        {chat.map((msg, i) => (
-          <div key={i} className={`message ${msg.type}`}>
-            <strong>{msg.type === "user" ? "You" : "Bot"}:</strong> {msg.text}
-          </div>
-        ))}
-        {loading && <div className="message bot">Bot is typing...</div>}
+    <>
+      {/* <ChatResponseTable res={chat} /> */}
+
+      <div className="Chatbot">
+        <div className="chat-box">
+          {chat.map((msg, i) => (
+            <div key={i} className={`message ${msg.type}`}>
+              <strong>{msg.type === "user" ? "You" : "Bot"}:</strong> {msg.text}
+            </div>
+          ))}
+          {loading && <div className="message bot">Bot is typing...</div>}
+        </div>
+        <div className="input-box">
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyPress={(e) => e.key === "Enter" && sendMessage()}
+            placeholder="Type your message..."
+          />
+          <button onClick={sendMessage} disabled={loading}>
+            Send
+          </button>
+        </div>
       </div>
-      <div className="input-box">
-        <input
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          onKeyPress={(e) => e.key === "Enter" && sendMessage()}
-          placeholder="Type your message..."
-        />
-        <button onClick={sendMessage} disabled={loading}>
-          Send
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
 
